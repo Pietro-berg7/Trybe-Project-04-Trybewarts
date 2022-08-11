a1383086ef5eb754e75bf808ed0dfe77c84f9ff1
Guilherme Patrick <guilherme@mgesportes.com.br>
2022-08-11 16:37:04 -0300

    Requisito 4 concluido

--- script.js
+++ script.js
@@ -1,13 +1,12 @@
 const botaoLogin = document.getElementById('botao-login');
 const inputEmail = document.getElementsByName('email')[0];
-const inputSenha = document.getElementsByName('senha')[0];
+const inputSenha = document.getElementsByName('password')[0];
 
 botaoLogin.addEventListener('click', verificaLogin);
 
 function verificaLogin() {
-    if (inputEmail.value !== 'tryber@teste.com' || inputSenha.value !== '123456') {
-        alert('Email ou senha inválidos.');
-    } else {        
-        alert('Olá, Tryber!');
+    if (inputEmail.value === 'tryber@teste.com' && inputSenha.value === '123456') {
+        return alert('Olá, Tryber!');
     }
+    alert('Email ou senha inválidos.');
 }
